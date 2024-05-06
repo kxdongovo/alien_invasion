aa52bf96c2e8aaac424e8c35fb918ba68ec53fa9
GoTG <15632151782@163.com>
2024-05-06 17:55:58 +0800

    新增子弹射击

--- settings.py
+++ settings.py
@@ -8,4 +8,9 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (173, 216, 230)
         #飞船设置
-        self.ship_speed = 1.5
+        self.ship_speed = 5.5
+        #子弹设置
+        self.bullet_speed = 8.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (0, 0, 0)
