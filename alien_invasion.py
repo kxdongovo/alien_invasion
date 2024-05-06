928d3d1e7dbabc6f6eca3b117f2d6e3410a539a9
GoTG <15632151782@163.com>
2024-05-06 12:25:09 +0800

    提交

--- alien_invasion.py
+++ alien_invasion.py
@@ -26,20 +26,28 @@ class AlienIncasion:
     def run_game(self):
         #开始游戏的住循环
         while True:
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
             self.clock.tick(60)
-            # 监听键盘和鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
 
-            #每次循环都重绘屏幕
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
 
             #让最近绘制的屏幕可见
             pygame.display.flip()
             self.clock.tick(60)
+
+    def _check_events(self):
+        #响应按键和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        # 每次循环都重绘屏幕
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
+
+
 
 if __name__ == '__main__':
     #创建游戏实例并运行游戏
